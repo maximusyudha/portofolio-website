47fdb23e68bf21d0a51c1f6d42ae2dd583f7e7b6
Maximus Yudha <maximusyudha781@gmail.com>
2024-04-04 01:44:08 +0700

    First Commit

---
+++ components/main/Hero.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import HeroContent from "../sub/HeroContent";
+import { Project } from "./Project";
+
+
+const Hero = () => {
+  return (
+    <div className="relative flex flex-col h-full w-full" id="about-me">
+      {/* <video
+        autoPlay
+        muted
+        loop
+        className="rotate-180 absolute top-[-340px]  h-full w-full left-0 z-[1] object-cover "
+      >
+        <source src="/blackhole.webm" type="video/webm" />
+      </video> */}
+      <HeroContent />
+      <Project />
+    </div>
+  );
+};
+
+export default Hero;
---
+++ components/main/Project.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import ProjectContent from "../sub/ProjectContent";
+
+export const Project = () => {
+  return (
+    <div className="">
+      <ProjectContent dummyProjects={[]} />
+    </div>
+  );
+};
